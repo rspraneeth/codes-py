d36c38db4befdd35a0ec84930248e414e2c499a4
rspraneeth <rspraneeth.rsp@gmail.com>
2022-06-10 13:47:45 +0530

    Higher lower game https://www.higherlowergame.com/

---
+++ HigherLowerGame/main.py
@@ -0,0 +1,40 @@
+from art import logo, vs
+from game_data import data
+from random import choice
+print(logo)
+def check_b_equals_a(a, b):
+    if b == a:
+        b = choice(data)
+        check_b_equals_a(a, b)
+    return b
+
+a_person = choice(data)
+scr = 0
+
+# check_b_equals_a(a, b)
+def game(a, score):
+    b = choice(data)
+    b = check_b_equals_a(a, b)
+    print(f"Compare A: {a['name']}, a {a['description']}, from {a['country']}")
+    print(vs)
+    print(f"Compare B: {b['name']}, a {b['description']}, from {b['country']}")
+    opt = input("Who has more followers A/B: ").lower()
+    if opt == 'a':
+        if a['follower_count'] >= b['follower_count']:
+            score += 1
+            a = b
+            print(f"You're right! Current score: {score}")
+            game(a, score)
+        else:
+            print(f"Sorry, that's wrong! Final score: {score} ")
+    elif opt == 'b':
+        if a['follower_count'] <= b['follower_count']:
+            score += 1
+            a = b
+            print(f"You're right! Current score: {score}")
+            game(a, score)
+        else:
+            print(f"Sorry, that's wrong! Final score: {score} ")
+
+game(a_person, scr)
+
