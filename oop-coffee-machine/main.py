7f0b2be69e6cebf1e669ee9f5c8ccb2b6f7f6c4f
rspraneeth <rspraneeth.rsp@gmail.com>
2022-06-13 22:07:34 +0530

    coffee machine program using OOP

---
+++ oop-coffee-machine/main.py
@@ -0,0 +1,24 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+is_on = True
+menu = Menu()
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like {options}: ")
+    if choice in options:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
+    elif choice == 'off':
+        is_on = False
+    elif choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        print("Select a proper item")
