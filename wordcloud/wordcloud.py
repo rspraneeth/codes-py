ce3aefc22b4d8d605c6c172b13081a6a8a3c165d
rspraneeth <rspraneeth.rsp@gmail.com>
2022-06-14 20:44:53 +0530

    Word cloud code that generates image based on no of times a word is present in string

--- wordcloud/wordcloud.py
+++ wordcloud/wordcloud.py
@@ -27,26 +27,16 @@ file_contents = "This means that bob refers to an object with type Turtle as def
 
 def calculate_frequencies(file_contents):
     # Here is a list of punctuations and uninteresting words you can use to process your text
-    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
-                           "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its",
-                           "they", "them", \
-                           "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be",
-                           "been", "being", \
-                           "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when",
-                           "where", "how", \
-                           "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very",
-                           "can", "will", "just"]
 
     # LEARNER CODE START HERE
     file_contents = file_contents.split(' ')
-    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my",
                            "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its",
-                           "they", "them", \
+                           "they", "them",
                            "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be",
-                           "been", "being", \
+                           "been", "being",
                            "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when",
-                           "where", "how", \
+                           "where", "how",
                            "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very",
                            "can", "will", "just", ""]
 
